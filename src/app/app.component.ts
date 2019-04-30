b188833c27d8be75086f6ff7b4db58516c563aac
emakuthi <emakuthi@gmail.com>
2019-04-30 14:54:06 +0300

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,11 @@ import { Goal } from './goal';
 export class AppComponent {
   title ='Goals';
   goals = [
-    new Goal(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
-    new Goal(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Goal(4, 'Get Dog Food','Pupper likes expensive sancks'),
-    new Goal(5, 'Solve math homework','Damn Math'),
-    new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+    new Goal(1,'Watch Finding Nemo','Find an online version and watch merlin find his son', new Date (2018,3,14)),
+    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot', new Date (2018,3,14)),
+    new Goal(3, 'Get new Phone Case','Diana has her birthday coming up soon', new Date (2018,3,14)),
+    new Goal(4, 'Get Dog Food','Pupper likes expensive sancks', new Date (2018,3,14)),
+    new Goal(5, 'Solve math homework','Damn Math', new Date (2018,3,14)),
+    new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord', new Date (2018,3,14)),
 ]
 }
